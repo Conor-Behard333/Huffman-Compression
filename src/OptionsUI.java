e3fd8fea31d6d26f858faa2ed69543eb2b3f44e2
Conor <Conorbehard@hotmail.co.uk>
2021-02-19 14:04:04 +0000

    OptionsUI looks nicer

--- src/OptionsUI.java
+++ src/OptionsUI.java
@@ -1,4 +1,5 @@
 import Huffman.HuffmanTree;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
@@ -19,20 +20,63 @@ public class OptionsUI {
         Stage stage = new Stage();
         stage.setTitle(title);
         stage.setResizable(false);
+        stage.setOnCloseRequest(event -> System.exit(0));//exit program if the main window is closed
 
-        Text select = new Text("File: ");
+        Text select = getTextElement("File: ");
         Button selectBtn = getSelectFileButton(stage);
         HBox hBox1 = new HBox();
         hBox1.getChildren().addAll(select, selectBtn);
 
-
-        Text selectOut = new Text("Select output location: ");
+        Text selectOut = getTextElement("Select output location: ");
         Button selectDirBtn = getSelectDirButton(stage);
         HBox hBox2 = new HBox();
         hBox2.getChildren().addAll(selectOut, selectDirBtn);
 
-        Button ok = new Button(title);
-        ok.setOnAction(event -> {
+        Button okButton = createOkButton(title, compress);
+        Button backButton = createBackButton(stage);
+        HBox hBox3 = new HBox();
+        hBox3.getChildren().addAll(backButton, okButton);
+
+        VBox root = getVBox(hBox1, hBox2, hBox3);
+
+        stage.setScene(new Scene(root, 600, 300));
+        stage.show();
+    }
+
+    private Text getTextElement(String s) {
+        Text selectOut = new Text(s);
+        selectOut.setStyle("-fx-font-size: 30");
+        selectOut.setTranslateY(5);
+        return selectOut;
+    }
+
+    private VBox getVBox(HBox hBox1, HBox hBox2, HBox hBox3) {
+        VBox root = new VBox();
+        hBox1.setPadding(new Insets(20, 0, 20, 10));
+        hBox2.setPadding(new Insets(0, 0, 20, 10));
+        hBox3.setPadding(new Insets(0, 0, 20, 50));
+        hBox2.setTranslateY(20);
+        hBox3.setTranslateY(50);
+        root.getChildren().addAll(hBox1, hBox2, hBox3);
+        return root;
+    }
+
+    private Button createBackButton(Stage stage) {
+        Button backButton = new Button("Back");
+        backButton.setPrefSize(250, 50);
+        backButton.setStyle("-fx-font-size: 30");
+        backButton.setOnAction(event -> {
+            new HomeUI().start(new Stage());
+            stage.close();
+        });
+        return backButton;
+    }
+
+    private Button createOkButton(String title, boolean compress) {
+        Button okButton = new Button(title);
+        okButton.setPrefSize(250, 50);
+        okButton.setStyle("-fx-font-size: 30");
+        okButton.setOnAction(event -> {
             HuffmanTree huffman = new HuffmanTree();
             if (fileSelected != null && outputDir != null) {
                 try {
@@ -48,20 +92,7 @@ public class OptionsUI {
                 }
             }
         });
-
-        Button cancel = new Button("Back");
-        cancel.setOnAction(event -> {
-            new HomeUI().start(new Stage());
-            stage.close();
-        });
-        HBox hBox3 = new HBox();
-        hBox3.getChildren().addAll(cancel, ok);
-
-        VBox root = new VBox();
-        root.getChildren().addAll(hBox1, hBox2, hBox3);
-        stage.setOnCloseRequest(event -> System.exit(0));//exit program if the main window is closed
-        stage.setScene(new Scene(root, 300, 250));
-        stage.show();
+        return okButton;
     }
 
     private Button getSelectFileButton(Stage stage) {
@@ -70,9 +101,13 @@ public class OptionsUI {
                 new FileChooser.ExtensionFilter("Text Files", "*.txt")
         );
         Button button = new Button("Select txt File");
+        button.setPrefSize(200, 60);
+        button.setStyle("-fx-font-size: 18");
         button.setOnAction(event -> {
             fileSelected = fc.showOpenDialog(stage);
-            button.setText(fileSelected.getName());
+            if (fileSelected != null) {
+                button.setText(fileSelected.getName());
+            }
         });
         return button;
     }
@@ -80,10 +115,14 @@ public class OptionsUI {
     private Button getSelectDirButton(Stage stage) {
         DirectoryChooser dc = new DirectoryChooser();
         Button button = new Button("Select Output directory");
+        button.setPrefSize(250, 60);
+        button.setStyle("-fx-font-size: 18");
         button.setOnAction(event -> {
             outputDir = dc.showDialog(stage);
-            button.setText(outputDir.getPath());
-            button.setTooltip(new Tooltip(button.getText()));
+            if (outputDir != null) {
+                button.setText(outputDir.getPath());
+                button.setTooltip(new Tooltip(button.getText()));
+            }
         });
         return button;
     }
